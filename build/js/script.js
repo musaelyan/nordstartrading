d1a724a9bbf2ce47844cd1d290e9c51639426128
Anastasia Musaelyan <anastasia.musaelyan@gmail.com>
2016-12-11 15:14:16 +0300

    стилизованы контакты в хедере

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,22 @@
 
 'use strict';
 
-(function() {
+var aboutBlockHeight = document.querySelector(".about__block--text").clientHeight;
+var aboutImage = document.querySelector(".about__block--image");
 
+var advantagesBlockHeight = document.querySelector(".advantages__block--text").clientHeight;
+var advantagesImage = document.querySelector(".advantages__block--image");
+
+console.log(aboutBlockHeight);
+console.log(aboutImage);
+
+
+window.onload = (function() {
+  aboutImage.style.height = aboutBlockHeight + 'px';
+  advantagesImage.style.height = advantagesBlockHeight + 'px';
 })();
+
+window.addEventListener('resize', function(){
+  aboutImage.style.height = aboutBlockHeight + 'px';
+  advantagesImage.style.height = advantagesBlockHeight + 'px';
+}, true);
